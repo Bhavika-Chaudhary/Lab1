0cfca7f2dd7475fe6cb1ec70fbfe2e4964f5996f
Bhavika Chaudhary <bhavikavedvan02@gmail.com>
2025-10-15 16:40:33 +0200

    Added some more code

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
     public static void main() {
         System.out.println("Hello Vistula!");
         System.out.println("This is my first time using git and github and I am enjoying it ;) .");
+        String city = "Warsaw";
+        System.out.println("I am currently living in " + city);
+        int a = 8;
+        int b = 2;
+        System.out.println("If a = 8 , b = 2 ,then Sum of a and b is : " +(a+b));
     }
 }
